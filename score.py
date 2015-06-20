30bc4ec2b91f9c88c08914ad121b6a7d4919ef86
John Mount <jmount@win-vector.com>
2015-06-20 16:52:42 -0700

    try to use Caffe net to score

--- score.py
+++ score.py
@@ -1,11 +1,51 @@
 
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
-print(str(sys.argv))
-inF = sys.argv[1]
+
+print str(sys.argv)
+IMAGE_FILE = sys.argv[1]
 outF = sys.argv[2]
-score  = 0.7
-f1 =  open(outF, 'w')
+
+
+# Make sure that caffe is on the python path:
+caffe_root = '../'  # this file is expected to be in {caffe_root}/examples
+import sys
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+
+# Set the right path to your model definition file, pretrained model weights,
+# and the image you would like to classify.
+MODEL_FILE = '../models/bvlc_reference_caffenet/deploy.prototxt'
+PRETRAINED = '../models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+
+
+with open('../data/ilsvrc12/synset_words.txt') as f:
+    content = f.readlines()
+
+
+
+net = caffe.Classifier(MODEL_FILE, PRETRAINED,
+                       mean=np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1),
+                       channel_swap=(2,1,0),
+                       raw_scale=255,
+                       image_dims=(256, 256))
+
+input_image = caffe.io.load_image(IMAGE_FILE)
+
+prediction = net.predict([input_image])  # predict takes any number of images, and formats them for the Caffe net automatically
+pclass = prediction[0].argmax()
+print 'predicted class:', pclass
+print 'predicted class name:', content[pclass]
+
+# crabs
+score = prediction[120] + prediction[121] + prediction[122] + prediction[123] + prediction[126]
+
+print "SCORE",score
+
+f1 = open(outF, 'w')
 f1.write(str(score))
 f1.write('\n')
 f1.close()
