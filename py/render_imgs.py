05ff930db8488bdaf96394bfd60af6ecaa1c9b51
John Mount <jmount@win-vector.com>
2024-05-27 12:47:59 -0700

    Add exif data

--- py/render_imgs.py
+++ py/render_imgs.py
@@ -4,6 +4,8 @@ import os
 from multiprocessing import cpu_count, Pool
 
 import numpy as np
+from PIL import ExifTags, Image
+
 
 from render_gart import render_gart
 from farchive import farchive
@@ -12,16 +14,33 @@ from farchive import farchive
 img_dir = 'imgs'
 
 
+def get_tag_number(tag_name):
+    for tag, name in ExifTags.TAGS.items():
+        if name == tag_name:
+            return tag
+    return None
+
+tag_ImageDescription = get_tag_number('ImageDescription')
+
+
 def mk_img_job(idxs):
     for i in idxs:
         formula_str = farchive[i]
         img = render_gart(
             formula_str,
-            img_height=1600,
-            img_width=2560,
+            img_height= 2160,
+            img_width= 3840,
             aa_scale=2,
         )
-        img.save(f"{img_dir}/{i:05d}.jpeg")
+        exif_dict = img.getexif()
+        comment_text = f"https://github.com/JohnMount/Gart {i} {formula_str}"
+        exif_dict[tag_ImageDescription] = comment_text
+        img.save(
+            f"{img_dir}/{i:05d}.jpeg",
+            subsampling=0,
+            quality=95,
+            exif=exif_dict,
+            )
 
 
 if __name__ == "__main__":
